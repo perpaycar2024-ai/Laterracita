53a4537df59130eccfb12698eef83bf5a02e271f
perpaycar2024-ai <perpaycar2024@gmail.com>
2026-04-01 20:22:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// VERSIÓN 7 — fuerza actualización borrando cachés anteriores
+// VERSIÓN 8 — fuerza actualización borrando cachés anteriores
 const CACHE_NAME = 'jardin-raul-v4';
 const ASSETS = [
   './Index.html',
